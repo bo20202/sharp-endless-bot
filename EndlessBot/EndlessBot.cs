def46fb7974014b8b95b443b46626c0627f18c43
bo20202 <bo20202@yandex.ru>
2017-05-15 11:37:15 +0300

    Some refactoring

--- EndlessBot/EndlessBot.cs
+++ EndlessBot/EndlessBot.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using BotCore.Configuration;
+using BotCore.Services.ServerMonitoring;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
@@ -11,32 +12,22 @@ namespace BotCore
 {
     public class EndlessBot
     {
-        private readonly DiscordSocketClient _client;
+        private DiscordSocketClient _client;
         private CommandHandler _handler;
 
-        public EndlessBot()
+        public async Task Start()
         {
             Config.LoadConfig();
             _client = new DiscordSocketClient();
             _client.Log += Log;
-            _client.Connected += () =>
-            {
-                Console.WriteLine($"Connected with {_client.CurrentUser.Username}");
-                return Task.CompletedTask;
-            };
 
-        }
-
-        public async void Start()
-        {
-            
             await _client.LoginAsync(TokenType.Bot, Config.Token);
             await _client.StartAsync();
             var provider = ConfigureBotServices();
 
             _handler = new CommandHandler(provider);
             await _handler.ConfigureAsync();
-        }
+        }   
 
 
         private IServiceProvider ConfigureBotServices()
@@ -44,12 +35,12 @@ namespace BotCore
             var services = new ServiceCollection()
                 .AddSingleton(_client)
                 .AddSingleton(new CommandService(
-                    new CommandServiceConfig {CaseSensitiveCommands = false, ThrowOnError = false}));
+                    new CommandServiceConfig {CaseSensitiveCommands = false, ThrowOnError = false}))
+                .AddSingleton<ServerMonitoringService>();
 
             var provider = services.BuildServiceProvider();
             return provider;
         }
-
         private static Task Log(LogMessage msg)
         {
             Console.WriteLine(msg.ToString());
