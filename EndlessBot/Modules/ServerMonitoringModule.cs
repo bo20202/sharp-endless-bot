ec7fb8964b88c66b14b2569dd09e00c6eaa1ca3d
bo20202 <bo20202@yandex.ru>
2017-05-15 11:38:01 +0300

    Temp. remove user attribute

--- EndlessBot/Modules/ServerMonitoringModule.cs
+++ EndlessBot/Modules/ServerMonitoringModule.cs
@@ -1,11 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using BotCore.Preconditions;
 using BotCore.Services.ServerMonitoring;
 using Discord.Commands;
 
 namespace BotCore.Modules
 {
-    public class ServerMonitoringModule : ModuleBase
+    public class ServerMonitoringModule : ModuleBase<SocketCommandContext>
     {
         private readonly ServerMonitoringService _service;
 
@@ -15,10 +16,27 @@ namespace BotCore.Modules
         }
 
         [Command("start")]
-        [InAllowedUsers]
         public async Task Start()
         {
-            
+            await Context.Channel.SendMessageAsync("Start");
+            _service.StartMonitoring();
+            _service.GotServerData += GotDataHandler;
+        }
+
+        [Command("stop")]
+        public async Task Stop()
+        {
+            await Context.Channel.SendMessageAsync("Stop");
+            _service.StopMonitoring();
+            _service.GotServerData -= GotDataHandler;
+        }
+
+        private void GotDataHandler(object sender, ServerMonitoringEventArgs args)
+        {
+            Task.Run(async () =>
+            {
+                await Context.Channel.SendMessageAsync("Players:" + args.ServerInfo.Players);
+            });
         }
     }
 }
