98d0cff5210253cde936690b66c9533703f3ada3
bo20202 <bo20202@yandex.ru>
2017-05-16 00:34:50 +0300

    Update server info model

--- EndlessBot/Configuration/ServerInfo.cs
+++ EndlessBot/Configuration/ServerInfo.cs
@@ -2,7 +2,10 @@
 {
     public class ServerInfo
     {
+        public Server Server { get; set; }
         public int Players { get; set; }
         public int Admins { get; set; }
+        public bool IsOnline { get; set; }
+
     }
 }
